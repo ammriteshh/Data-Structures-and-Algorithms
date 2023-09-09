48636a34ef92bc2cb9c4a02699aa88ce12254618
ammriteshh <amritesh6767@gmail.com>
2023-09-09 13:16:33 +0530

    Sorting

--- STL/Pairs.cpp
+++ STL/Pairs.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 void explainPairs() {
-    pair<int, int> p = {1, 3};
+    pair<int, int> p1 = {1, 3};
 
-    cout << p.first <<" " << p.second; //1 and 3
+    cout << p1.first <<" " << p1.second; //1 and 3
 
-    pair<int, pair<int, int>> p = {1, {3, 4}};
+    pair<int, pair<int, int>> p2 = {1, {3, 4}};
 
-    cout<< p.first << " " << p.second.second << " " << p.second.first;  //1 << 4 << 3
+    cout<< p2.first << " " << p2.second.second << " " << p2.second.first;  //1 << 4 << 3
 
     pair<int, int> arr[] = { {1, 2}, {2, 5}, {5, 1}};
 
     cout << arr[1].second;  //5
 }
+
+
--- STL/Vectors.cpp
+++ STL/Vectors.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 void explainVector() {
 
-    vector<int> v;
+    vector<int> v; //VectorName<datatype> datatypename; (it creates an empty container)
 
-    v.push_back(1);
-    v.emplace_back(2);
+    v.push_back(1); //pushback = add(insert), it inserts 1 in the empty container
+    v.emplace_back(2); //emplaceback is similar to pushback, it inserts 2 so now the empty container becomes {1, 2}
 
-    vector<pair<int, int>>vec;
+    vector<pair<int, int>>vec; //vector of pair datatype
 
-    v.push_back({1, 2});  //insert
-    v.emplace_back(1, 2);  
+    // v.push_back({1, 2});  //must be in curly braces
+    v.emplace_back(1, 2);  // no need of curly braces
 
     vector<int> v(5, 100); //{100,100,100,100,100}
 
-    vector<int> v(5);
+    vector<int> v(5); //{0,0,0,0,0}, or garbage value
 
     vector<int> v1(5, 20);  //{20,20,20,20,20}
     vector<int> v2(v1); //copy of upper line
