5c74e50603448ddb90d70fac6449177ac1012497
ammriteshh <amritesh6767@gmail.com>
2023-10-28 14:02:19 +0530

    Mode Of Inheritance

--- OOPS/ModeOfInheritance.cpp
+++ OOPS/ModeOfInheritance.cpp
@@ -31,3 +31,8 @@ class Chile3: protected Parent{
     //y will be protected
     //z will be inaccessable
 };
+
+// The capability of a class to derive properties and characteristics from another class is called Inheritance.
+
+// Sub Class: The class that inherits properties from another class is called Subclass or Derived Class. 
+// Super Class: The class whose properties are inherited by a subclass is called Base Class or Superclass. 
